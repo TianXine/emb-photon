8c8fc000483684355f770c220043cb0191e55e11
wangjianwei <wangjianwei@roblab.geely.com>
2022-02-20 18:44:55 +0800

    Finished project template starter.

---
+++ src/starter/main.c
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  main
+ *
+ *        Version:  1.0
+ *        Created:  02/20/2022 03:29:35 PM
+ *       Revision:  none
+ *
+ *         Author:  xine (xine), xinedev@outlook.com
+ *   Organization:  xinedev
+ *
+ * =====================================================================================
+ */
+
+void SystemInit()
+{
+}
+
+int main()
+{
+	while(1){
+	}
+}
+
---
+++ src/starter/stm32f4xx.h
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  stm32f4xx.h
+ *
+ *    Description:  stm32f4xx specific data
+ *
+ *        Version:  1.0
+ *        Created:  02/20/2022 03:30:53 PM
+ *       Revision:  none
+ *
+ *         Author:  xine (xine), xinedev@outlook.com
+ *   Organization:  xinedev
+ *
+ * =====================================================================================
+ */
+#ifndef STM32F4XX_H_
+#define STM32F4XX_H_
+
+extern void SystemInit(void);
+
+#endif /* end of STM32F4XX_H_ */
+
