3d90eaf86917b7e196559684b0dd112d0742a5a8
xine <xinedev@outlook.com>
2022-02-24 19:45:37 +0800

    Updated download script.

--- src/01_flatled/main.c
+++ src/01_flatled/main.c
@@ -19,33 +19,37 @@
 
 typedef unsigned int u32;
 
-void SystemInit() {}
-
-static void delay(u32 i) {
-  while (i--)
-    ;
+void SystemInit()
+{
 }
 
-int main() {
-  // Enable RCC for GPIOF
-  RCC_AHB1ENR |= ( 0x01 << 5 | 0x01 << 6 );
-  GPIOF_MODER = (1 << (2 * 9) | 1 << (2*10)); // set GPIOF to general output
-  GPIOG_MODER = (1 << (2 * 9) | 1 << (2*10)); // set GPIOG to general output
-  char keepRunning = 1;
-  while (keepRunning) {
-	// LED0
-    GPIOF_BSRR = (1 << (16 + 9));
-    delay(0xFFFFF);
-    GPIOF_BSRR = (1 << 9);
-    delay(0xFFFFF);
+static void delay(u32 i)
+{
+    while (i--)
+        ;
+}
 
-	// LED1
-	GPIOG_BSRR = (1 << (16+10));
-    delay(0xFFFFF);
-    GPIOG_BSRR = (1 << 10);
-    delay(0xFFFFF);
-  }
-  return 0;
+int main()
+{
+    // Enable RCC for GPIOF
+    RCC_AHB1ENR |= (0x01 << 5 | 0x01 << 6);
+    GPIOF_MODER      = (1 << (2 * 9) | 1 << (2 * 10)); // set GPIOF to general output
+    GPIOG_MODER      = (1 << (2 * 9) | 1 << (2 * 10)); // set GPIOG to general output
+    char keepRunning = 1;
+    while (keepRunning) {
+        // LED0
+        GPIOF_BSRR = (1 << (16 + 9));
+        delay(0xFFFFF);
+        GPIOF_BSRR = (1 << 9);
+        delay(0xFFFFF);
+
+        // LED1
+        GPIOG_BSRR = (1 << (16 + 10));
+        delay(0xFFFFF);
+        GPIOG_BSRR = (1 << 10);
+        delay(0xFFFFF);
+    }
+    return 0;
 }
 
 /**
@@ -54,6 +58,6 @@ int main() {
  * */
 void Reset_Handler()
 {
-	SystemInit();
-	main();
+    SystemInit();
+    main();
 }
